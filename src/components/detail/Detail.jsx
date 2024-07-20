dc0d97c67797c4c5333c5a5cf22b0fba04599f0a
Codedys <oberegeraldjackson@gmail.com>
2024-07-20 15:44:15 +0300

    Details section UI done

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,82 @@ import "./detail.css"
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor sit amet  .</p>
+      </div>
+
+      <div className="info">
+
+        <div className="options">
+          <div className="title">
+            <span>Chat settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="options">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="options">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+
+        <div className="photo">
+          <div className="photoitem">
+            <div className="photodetail">
+            <img src="./avatar.png" alt="" />
+            <span>Photo_2024_.png</span>
+            </div>
+            <img src="./download.png" alt="" className="icon"/>
+          </div>
+          <div className="photoitem">
+            <div className="photodetail">
+            <img src="./avatar.png" alt="" />
+            <span>Photo_2024_.png</span>
+            </div>
+            <img src="./download.png" alt="" className="icon" />
+          </div>
+          <div className="photoitem">
+            <div className="photodetail">
+            <img src="./avatar.png" alt="" />
+            <span>Photo_2024_.png</span>
+            </div>
+            <img src="./download.png" alt="" className="icon" />
+          </div>
+          <div className="photoitem">
+            <div className="photodetail">
+            <img src="./avatar.png" alt="" />
+            <span>Photo_2024_.png</span>
+            </div>
+            <img src="./download.png" alt="" className="icon"/>
+          </div>
+        </div>
+        </div>
+
+
+        <div className="options">
+          <div className="title">
+            <span>shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <button>Block user</button>
+
+      </div>
+
+
+    </div>
   )
 }
 
